276541adbc6c3c336bb6261ee730ff8fd5179ecb
915287336@qq.com <shuye520>
2019-05-19 20:00:14 +0800

    Initial commit

---
+++ app/src/main/java/fangdichan/com/fangdichan/LoginActivity.java
@@ -0,0 +1,64 @@
+package fangdichan.com.fangdichan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import org.litepal.LitePal;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.text_zhuce)
+    TextView textZhuce;
+    @BindView(R.id.login_phone)
+    EditText loginPhone;
+    @BindView(R.id.login_pass)
+    EditText loginPass;
+    @BindView(R.id.submit)
+    Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        inittooler();
+    }
+
+    private void inittooler() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("登录");
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
+    }
+
+    @OnClick({R.id.text_zhuce,R.id.submit})
+    public void onViewClicked(View view) {
+
+    }
+
+}
